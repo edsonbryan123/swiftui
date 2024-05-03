7abe21287dd4a56957dc31eb40543fdb75d5dce0
Edsonbryan <nfek361@gmail.com>
2024-05-03 08:25:51 +0700

    Initial Commit

---
+++ swiftui/swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiApp.swift
+//  swiftui
+//
+//  Created by student on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
